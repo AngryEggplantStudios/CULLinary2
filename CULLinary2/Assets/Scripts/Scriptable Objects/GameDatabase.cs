a71a7ee5d942c480d96f6cffe381c39c1aee98a6
Darren Sim <klassiclegend@gmail.com>
2021-05-29 22:53:24 +0800

    Write in game data

---
+++ CULLinary2/Assets/Scripts/Scriptable Objects/GameDatabase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game Data containing all scriptable objects to be loaded before the game starts.
+/// </summary>
+[CreateAssetMenu(fileName = "New Database", menuName = "Game Database")]
+public class GameDatabase : ScriptableObject
+{
+    public List<Ingredient> allIngredients;
+}
---
+++ CULLinary2/Assets/Scripts/System/PlayerKeybinds.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum KeybindAction
+{
+    Interact,
+    OpenInventory,
+    OpenRecipeBook,
+    Trash,
+    CloseMenu,
+    StatsMenu
+}
+
+/// <summary>
+/// Player keybinds for actions
+/// </summary>
+public class PlayerKeybinds
+{
+    private static Dictionary<KeybindAction, KeyCode> keybinds = new Dictionary<KeybindAction, KeyCode>
+    {
+        { KeybindAction.Interact, KeyCode.F },
+        { KeybindAction.OpenInventory, KeyCode.I },
+        { KeybindAction.OpenRecipeBook, KeyCode.R },
+        { KeybindAction.Trash, KeyCode.Delete },
+        { KeybindAction.CloseMenu, KeyCode.Escape },
+        { KeybindAction.StatsMenu, KeyCode.Q }
+    };
+
+    /// <summary>
+    /// Returns a bool value based on whether the action was triggered by the player.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public static bool WasTriggered(KeybindAction key)
+    {
+        return Input.GetKeyDown(keybinds[key]);
+    }
+
+    /// <summary>
+    /// Changes keybind of an action given a keycode by the player. Returns true if the action is allowed, else false.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="keyCode"></param>
+    /// <returns></returns>
+    public static bool ChangeKeybind(KeybindAction key, KeyCode keyCode)
+    {
+        if (keybinds.ContainsValue(keyCode)) //Check if keycode is already being used by another action
+        {
+            return false;
+        }
+        keybinds[key] = keyCode;
+        return true;
+    }
+}
