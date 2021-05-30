944bf39ffb1195de3ddcfaaeeabf8d290b3fcf53
Darren Sim <klassiclegend@gmail.com>
2021-05-30 19:18:27 +0800

    Write in fade

---
+++ CULLinary2/Assets/Scripts/System/SceneIndexes.cs
@@ -0,0 +1,6 @@
+public enum SceneIndexes
+{
+    OPENING_CUTSCENE=1,
+    MAIN_MENU=2,
+    DUNGEON=3,
+}
---
+++ CULLinary2/Assets/Scripts/Util/FadeInFadeOut.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Taken from https://forum.unity.com/threads/simple-ui-animation-fade-in-fade-out-c.439825/
+/// </summary>
+public enum FadeAction
+{
+    FadeIn,
+    FadeOut,
+    FadeInAndOut,
+    FadeOutAndIn
+}
+
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    [Tooltip("The Fade Type.")]
+    [SerializeField] private FadeAction fadeType;
+
+    [Tooltip("the image you want to fade, assign in inspector")]
+    [SerializeField] private Image img;
+
+    [Tooltip("The amount of pause for the delay")]
+    [SerializeField] private float fadePause;
+
+
+    public void Start()
+    {
+        if (fadeType == FadeAction.FadeIn)
+        {
+
+            StartCoroutine(FadeIn());
+
+        }
+
+        else if (fadeType == FadeAction.FadeOut)
+        {
+
+            StartCoroutine(FadeOut());
+
+        }
+
+        else if (fadeType == FadeAction.FadeInAndOut)
+        {
+
+            StartCoroutine(FadeInAndOut());
+
+        }
+
+        else if (fadeType == FadeAction.FadeOutAndIn)
+        {
+
+            StartCoroutine(FadeOutAndIn());
+
+        }
+    }
+
+    // fade from transparent to opaque
+    IEnumerator FadeIn()
+    {
+
+        // loop over 1 second
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+    }
+
+    // fade from opaque to transparent
+    IEnumerator FadeOut()
+    {
+        // loop over 1 second backwards
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeInAndOut()
+    {
+        // loop over 1 second
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        //Temp to Fade Out
+        yield return new WaitForSeconds(fadePause);
+
+        // loop over 1 second backwards
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOutAndIn()
+    {
+        // loop over 1 second backwards
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        //Temp to Fade In
+        yield return new WaitForSeconds(fadePause);
+
+        // loop over 1 second
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+}
