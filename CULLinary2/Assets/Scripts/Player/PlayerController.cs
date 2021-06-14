86cf5400c5cc8334b85a1c1ed7e00a3a85ef2961
Darren Sim <klassiclegend@gmail.com>
2021-06-14 14:00:08 +0800

    Update PlayerController.cs

--- CULLinary2/Assets/Scripts/Player/PlayerController.cs
+++ CULLinary2/Assets/Scripts/Player/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : PlayerAction
     //Speed
     [SerializeField] private float walkSpeed = 10.0f;
     [SerializeField] private float runSpeed = 20.0f;
-    [SerializeField] private float turnSpeed = 10.0f;
+    [SerializeField] private float turnSpeed = 10.0f;   
 
     // Whether to invert movement directions and keyboard mapping
     [SerializeField] private bool invertKeys = false;
@@ -23,52 +23,21 @@ public class PlayerController : PlayerAction
     // Whether run is enabled
     [SerializeField] private bool canRun = true;
 
-    //Directions
-    private Vector3 direction;
-    private Vector3 normalizedDirection;
-    private Vector3 moveDirection;
-
-    // Helper variables for restaurant
-    private int directionMultiplier = 1;   // Restaurant camera is facing backwards
-    private bool isMovementAllowed = true; // Disable movement while cooking
-
-    // Disables movement of this player.
-    public void DisableMovement()
-    {
-        isMovementAllowed = false;
-    }
-
-    // Enables movement of this player.
-    public void EnableMovement()
-    {
-        isMovementAllowed = true;
-    }
-
     // Face a certain position in world coordinates.
     public void Face(Vector3 positionToLookAt)
     {
         OnPlayerInteract?.Invoke(positionToLookAt, turnSpeed);
     }
 
-    private void Start()
-    {
-        directionMultiplier = invertKeys ? -1 : 1;
-    }
-
     private void Update()
     {
-        if (!isMovementAllowed)
-        {
-            return;
-        }
 
         float moveVertical = Input.GetAxisRaw("Vertical");
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
 
-        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        normalizedDirection = direction.normalized * directionMultiplier;
-        float targetAngle = Mathf.Atan2(normalizedDirection.x, normalizedDirection.z) * Mathf.Rad2Deg;
-        moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+        Vector3 direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        float targetAngle = Mathf.Atan2(direction.normalized.x, direction.normalized.z) * Mathf.Rad2Deg;
+        Vector3 moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
 
         this.SetIsInvoking(true);
 
@@ -90,7 +59,7 @@ public class PlayerController : PlayerAction
 
         if (direction != Vector3.zero)
         {
-            OnPlayerRotate?.Invoke(normalizedDirection, turnSpeed);
+            OnPlayerRotate?.Invoke(direction.normalized, turnSpeed);
         }
     }
 
