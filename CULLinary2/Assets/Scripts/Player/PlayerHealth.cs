94bc92053a2fb9f6d0ce05605bb2a9deac013eab
Darren Sim <klassiclegend@gmail.com>
2021-06-26 21:49:58 +0800

    Write in handle hit

--- CULLinary2/Assets/Scripts/Player/PlayerHealth.cs
+++ CULLinary2/Assets/Scripts/Player/PlayerHealth.cs
@@ -9,6 +9,25 @@ public class PlayerHealth : MonoBehaviour {
   private bool isInvincible = false;
 
   private void Awake() {
-    int health = PlayerManager.instance ? PlayerManager.instance.currentHealth : 200;
+    int currentHealth = PlayerManager.instance ? PlayerManager.instance.currentHealth : 200;
+    int maxHealth = PlayerManager.instance ? PlayerManager.instance.maxHealth : 200;
+    healthBar.fillAmount = currentHealth / maxHealth;
+    healthText.text = currentHealth + "/" + maxHealth;
   }
+
+  public bool HandleHit(int damage) {
+    if (isInvincible) {
+      return false;
+    }
+    PlayerManager.instance.currentHealth -= damage;
+    healthBar.fillAmount = PlayerManager.instance.currentHealth / PlayerManager.instance.maxHealth;
+    healthText.text = PlayerManager.instance.currentHealth + "/" + PlayerManager.instance.maxHealth;
+    //Spawn damage counter
+    //Play Audio
+    //If health is 0, play game over scene and invoke game over event
+    return true;
+  }
+
+
+
 }
