a7dff0cd13fc29f4aa9c5ac8a0ca39229efffe65
MC <dandford@gmail.com>
2021-07-05 00:02:28 +0800

    Upgrade floating text

--- CULLinary2/Assets/Scripts/DevUtil/DeactivateAllEnviron.cs
+++ CULLinary2/Assets/Scripts/DevUtil/DeactivateAllEnviron.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class DeactivateAllEnviron : MonoBehaviour
 {
   private static List<GameObject> generatedRooms;
