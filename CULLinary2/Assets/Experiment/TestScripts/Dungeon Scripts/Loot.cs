652d8baebacd94b0a8b3054dd540326656fff422
MC <dandford@gmail.com>
2021-07-04 22:47:39 +0800

    Create NavMesh

---
+++ CULLinary2/Assets/Experiment/TestScripts/Dungeon Scripts/Loot.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loot : MonoBehaviour
+{
+    [SerializeField] private Item itemForLoot;
+
+    private GameObject player;
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip itemPickupSound;
+
+    [SerializeField] private GameObject itemPickupNotif_prefab;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        audioSource = player.transform.GetComponentInChildren<AudioSource>();
+    }
+
+    public Item GetItem()
+    {
+        return itemForLoot;
+    }
+
+    public void PickUp()
+    {
+        // Notif
+        GameObject itemPickupNotif = Instantiate(itemPickupNotif_prefab);
+        itemPickupNotif.transform.SetParent(GameObject.Find("UI").transform);
+        itemPickupNotif.transform.GetComponentInChildren<Image>().sprite = itemForLoot.icon;
+
+        // Sound
+        audioSource.clip = itemPickupSound;
+        audioSource.Play();
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log("loot colliding with: " + other.tag);
+        if ((other.CompareTag("Player") || other.CompareTag("MeleeWeapon"))) 
+        {
+           /* if (InventoryUI.instance.AddItem(this.GetItem()))
+            {
+                PickUp();
+            }*/
+        }
+    }
+}
--- CULLinary2/Assets/Scripts/Player/PlayerHealth.cs
+++ CULLinary2/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
+  [SerializeField] private AudioSource audioSource;
+  [SerializeField] private GameObject damageCounter_prefab;
+  [SerializeField] private GameObject gameOverUI;
   [SerializeField] private Image healthBar;
   [SerializeField] private Text healthText;
+  [SerializeField] private float invincibilityDurationSeconds;
+  [SerializeField] private GameObject model;
   private bool isInvincible = false;
+  private float currHealth;
+  private Renderer rend;
+  private Color[] originalColors;
+  private Color onDamageColor = Color.white;
+  private float invincibilityDeltaTime = 0.025f;
 
   private void Awake() {
     int currentHealth = PlayerManager.instance ? PlayerManager.instance.currentHealth : 200;
     int maxHealth = PlayerManager.instance ? PlayerManager.instance.maxHealth : 200;
     healthBar.fillAmount = currentHealth / maxHealth;
     healthText.text = currentHealth + "/" + maxHealth;
+    this.currHealth = currentHealth;
   }
 
   public bool HandleHit(int damage) {
@@ -25,9 +36,92 @@ public class PlayerHealth : MonoBehaviour {
     //Spawn damage counter
     //Play Audio
     //If health is 0, play game over scene and invoke game over event
+    SpawnDamageCounter(damage);
+    audioSource.Play();
+
+    if (this.currHealth <= 0)
+    {
+      //gameOverUI.GetComponent<GameManager>().GameOver();
+      //Die();
+      Debug.Log("Ded");
+    }
+    StartCoroutine(BecomeTemporarilyInvincible());
     return true;
   }
 
+  public float GetHealth()
+  {
+    return this.currHealth;
+  }
 
+  private void SetupFlash()
+  {
+    rend = model.GetComponentInChildren<Renderer>();
+    if (rend)
+    {
+      originalColors = new Color[rend.materials.Length];
+      for (var i = 0; i < rend.materials.Length; i++)
+      {
+        originalColors[i] = rend.materials[i].color;
+      }
+    }
+  }
+
+  private void SpawnDamageCounter(float damage)
+  {
+    GameObject damageCounter = Instantiate(damageCounter_prefab);
+    damageCounter.transform.GetComponentInChildren<Text>().text = damage.ToString();
+    //damageCounter.transform.SetParent(GameObject.FindObjectOfType<InventoryUI>().transform);
+    //damageCounter.transform.position = cam.WorldToScreenPoint(transform.position);
+    Debug.Log("I'm damaged by " + damage);
+  }
+
+  public void KnockbackPlayer(Vector3 positionOfEnemy)
+  {
+    //ToImplementKnockback
+    //StartCoroutine(KnockCoroutine(positionOfEnemy));
+    Vector3 forceDirection = transform.position - positionOfEnemy;
+    forceDirection.y = 0;
+    Vector3 force = forceDirection.normalized;
+    //dpl.KnockBack(force, 50, 3, true);
+  }
+
+  private IEnumerator KnockCoroutine(Vector3 positionOfEnemy)
+  {
+
+    Vector3 forceDirection = transform.position - positionOfEnemy;
+    Vector3 force = forceDirection.normalized;
+    gameObject.GetComponent<Rigidbody>().velocity = force * 4;
+    yield return new WaitForSeconds(0.3f);
+    gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+  }
+
+  private IEnumerator BecomeTemporarilyInvincible()
+  {
+    isInvincible = true;
+    bool isFlashing = false;
+    for (float i = 0; i < invincibilityDurationSeconds; i += invincibilityDeltaTime)
+    {
+      // Alternate between 0 and 1 scale to simulate flashing
+      if (isFlashing)
+      {
+        for (var k = 0; k < rend.materials.Length; k++)
+        {
+          rend.materials[k].color = onDamageColor;
+        }
+      }
+      else
+      {
+        for (var k = 0; k < rend.materials.Length; k++)
+        {
+          rend.materials[k].color = originalColors[k];
+        }
+      }
+      isFlashing = !isFlashing;
+      yield return new WaitForSeconds(invincibilityDeltaTime);
+    }
+    isInvincible = false;
+  }
 
 }
