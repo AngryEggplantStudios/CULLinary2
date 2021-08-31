1887939466d8fbfe70b3e93731ea52319d2f9f5d
MC <dandford@gmail.com>
2021-09-01 01:56:05 +0800

    Update rush enemy

---
+++ CULLinary2/Assets/RushEnemyScript.cs
@@ -0,0 +1,228 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RushEnemyScript : MonoBehaviour
+{
+    [SerializeField] private EnemyScript enemyScript;
+    [SerializeField] private RushEnemyAttack enemyAttack;
+    [SerializeField] private float distanceTriggered;
+
+    // Variables for idle
+    [SerializeField] private float idleTimer;
+    [SerializeField] private float wanderRadius;
+    [SerializeField] private float wanderTimer;
+    [SerializeField] private float minDist;
+    [SerializeField] private float distToCharge;
+
+    public LineRenderer debugLine;
+    // Variables for goingBackToStart
+    private float goingBackToStartTimer;
+
+    // Variables for roaming
+    private Vector3 roamPosition;
+
+    // Variables for attacking
+    private bool canAttack = true;
+    [SerializeField] private float timeBetweenAttacks;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+    private NavMeshHit navHit;
+    private Transform player;
+    private float timer;
+    private Vector3 startingPosition;
+    private Vector3? chargeDirection;
+    private bool amInAttackState;
+    [Tooltip("The minimum distance to stop. Has to be equal to Stopping distance. Cannot use stopping distance directly else navmesh agent will keep bumping into player/")]
+    private float reachedPositionDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Variables from EnemyScript
+        agent = enemyScript.getNavMeshAgent();
+        animator = enemyScript.getAnimator();
+        player = enemyScript.getPlayerReference();
+        enemyScript.onEnemyRoaming += EnemyRoaming;
+        enemyScript.onEnemyChase += EnemyChase;
+        enemyScript.onEnemyIdle += EnemyIdle;
+        enemyScript.onEnemyAttack += EnemyAttackPlayer;
+        enemyScript.onEnemyReturn += EnemyReturn;
+        reachedPositionDistance = agent.stoppingDistance;
+        amInAttackState = false;
+        startingPosition = transform.position;
+        timer = wanderTimer;
+        goingBackToStartTimer = 0;
+        enemyAttack = GetComponentInChildren<RushEnemyAttack>();
+    }
+
+    private void EnemyIdle()
+    {
+        Debug.Log(enemyAttack);
+        animator.SetBool("isMoving", false);
+        timer += Time.deltaTime;
+        enemyScript.FindTarget();
+        if (timer >= idleTimer)
+        {
+            Vector3 newPos = enemyScript.RandomNavSphere(startingPosition, wanderRadius, -1, minDist);
+            agent.SetDestination(newPos);
+            var points = new Vector3[2];
+
+            points[0] = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            points[1] = newPos;
+            debugLine.SetPositions(points);
+            timer = 0;
+            enemyScript.setStateMachine(State.Roaming);
+            roamPosition = newPos;
+        }
+    }
+
+    private void EnemyRoaming()
+    {
+        animator.SetBool("isMoving", true);
+        timer += Time.deltaTime;
+        enemyScript.FindTarget();
+        Vector3 distanceToFinalPosition = transform.position - roamPosition;
+        Debug.Log(distanceToFinalPosition);
+        //without this the eggplant wandering will be buggy as it may be within the Navmesh Obstacles itself
+        if (timer >= wanderTimer || distanceToFinalPosition.magnitude < reachedPositionDistance)
+        {
+            timer = 0;
+            Debug.Log("Has Set is moving to false");
+            animator.SetBool("isMoving", false);
+            enemyScript.setStateMachine(State.Idle);
+        }
+    }
+
+    private void EnemyChase()
+    {
+        Debug.Log("IN ENemy Chase");
+        Vector3 playerPositionWithoutYOffset = new Vector3(player.position.x, transform.position.y, player.position.z);
+        animator.SetBool("isMoving", true);
+        float directionVector = Vector3.Distance(transform.position, playerPositionWithoutYOffset);
+        if (directionVector <= reachedPositionDistance)
+        {
+            //transform.LookAt(playerPositionWithoutYOffset);
+            // Target within attack range
+            enemyScript.setStateMachine(State.AttackTarget);
+            // Add new state to attack player
+        }
+        else
+        {
+            Debug.Log("Setting destination");
+            agent.SetDestination(playerPositionWithoutYOffset);
+        }
+
+        if (Vector3.Distance(transform.position, player.position) > enemyScript.getStopChaseDistance() + 0.1f)
+        {
+            // Too far, stop chasing
+            enemyScript.setStateMachine(State.GoingBackToStart);
+        }
+    }
+
+    private void EnemyAttackPlayer()
+    {
+        Debug.Log("IN ENemy Attack");
+        Vector3 playerPositionWithoutYOffset = new Vector3(player.position.x, transform.position.y, player.position.z);
+        if (!chargeDirection.HasValue)
+		{
+            //means not charging
+            transform.LookAt(playerPositionWithoutYOffset);
+        }
+        float distanceOvershoot = Vector3.Distance(transform.position, playerPositionWithoutYOffset);
+        animator.SetBool("isMoving", false);
+        animator.ResetTrigger("attack");
+        if (canAttack == true)
+        {
+            animator.SetTrigger("attack");
+            amInAttackState = true;
+            canAttack = false;
+            StartCoroutine(DelayFire());
+        }
+
+        if (!enemyAttack.attackStarted())
+		{
+            Debug.Log("Setting charge diirection to null"); 
+            amInAttackState = false;
+            chargeDirection = null;
+/*            enemyScript.setStateMachine(State.Idle);*/
+        }
+        var points = new Vector3[2];
+
+        bool canStartCharging = enemyAttack.getCanDealDamage();
+        if (canStartCharging && !chargeDirection.HasValue)
+		{
+            Debug.Log("StartCharging");
+            //Initialize Start charging player
+            //chargeDirection = playerPositionWithoutYOffset;
+            chargeDirection = new Vector3(playerPositionWithoutYOffset.x, playerPositionWithoutYOffset.y, playerPositionWithoutYOffset.z) - transform.position;
+            Vector3 unitVectorForChargePosition = Vector3.Normalize(chargeDirection.Value);
+            
+            chargeDirection = unitVectorForChargePosition * distToCharge;
+            chargeDirection = chargeDirection + transform.position;
+
+            //charge towards nearest obstacle if hit
+            bool blocked = NavMesh.Raycast(transform.position, chargeDirection.Value, out navHit, NavMesh.AllAreas);
+
+            if (blocked)
+            {
+                //charge to the limit of nearest obstacle
+                chargeDirection = new Vector3(navHit.position.x - 0.01f, playerPositionWithoutYOffset.y, navHit.position.z - 0.01f);
+            }
+			points[0] = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+			points[1] = chargeDirection.Value;
+            //transform.position = chargeDirection.Value;
+			agent.SetDestination(chargeDirection.Value);
+            debugLine.SetPositions(points);
+        }
+        else if (canStartCharging && chargeDirection.HasValue)
+		{
+            //Destination of charge already set, continue charging
+            agent.SetDestination(chargeDirection.Value);
+        } 
+        else if (!canStartCharging && !amInAttackState)
+		{
+            //still haven't started charging and am not in attacking state
+            float directionVector = Vector3.Distance(transform.position, playerPositionWithoutYOffset);
+            /*            if (directionVector > agent.stoppingDistance && enemyScript.getCanMoveDuringAttack())
+                        {
+                            // Target within attack range
+                            Debug.Log("In Chasing target");
+                            enemyScript.setStateMachine(State.ChaseTarget);
+                        }*/
+        }
+        else
+		{
+            // am revving up to charge, do nothing
+		}
+    }
+
+    private void EnemyReturn()
+    {
+        Debug.Log("IN ENemy Return");
+        goingBackToStartTimer += Time.deltaTime;
+        animator.SetBool("isMoving", true);
+        float reachedPositionDistance = 1.0f;
+        transform.LookAt(startingPosition);
+        agent.SetDestination(startingPosition);
+        if (Vector3.Distance(transform.position, startingPosition) <= reachedPositionDistance || goingBackToStartTimer > 4.0f)
+        {
+            // Reached Start Position
+            animator.SetBool("isMoving", false);
+            enemyScript.setStateMachine(State.Idle);
+            goingBackToStartTimer = 0;
+            // Reset timer for roaming / idle time
+            timer = 0;
+        }
+    }
+
+
+    private IEnumerator DelayFire()
+    {
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        canAttack = true;
+    }
+
+
+}
