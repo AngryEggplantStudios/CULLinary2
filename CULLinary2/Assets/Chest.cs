9c9429fb88d22adada6d8c139ee93e726da6c460
WhiteLio <lio.lim@yahoo.com>
2021-10-17 01:57:00 +0800

    Add Empty Chest

---
+++ CULLinary2/Assets/Chest.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : PlayerInteractable
+{
+    public SpherePlayerCollider spCollider;
+    public GameObject[] spawnOnOpen;
+    public GameObject[] destroyOnOpen;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+    }
+
+    public override SpherePlayerCollider GetCollider()
+    {
+        return spCollider;
+    }
+
+    public override void OnPlayerInteract()
+    {
+        animator.enabled = true;
+        foreach (GameObject obj in spawnOnOpen)
+        {
+            Instantiate(obj, transform.position, transform.rotation);
+        }
+        foreach (GameObject obj in destroyOnOpen)
+        {
+            Destroy(obj);
+        }
+    }
+
+    public override void OnPlayerLeave()
+    {
+    }
+}
