a3f661f620da9e09779a25f3457c4fa3cc4df66a
alcen <49450743+alcen@users.noreply.github.com>
2021-10-18 20:44:21 +0800

    Add reversing

--- CULLinary2/Assets/Scripts/Player/CarController.cs
+++ CULLinary2/Assets/Scripts/Player/CarController.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private const string HORIZONTAL = "Horizontal";
-    private const string VERTICAL = "Vertical";
-
-    private float horizontalInput;
-    private float verticalInput;
-    private float currentSteerAngle;
-    private float currentBrakeForce;
-    private bool isBraking;
-
     [Header("Car Constants")]
     [SerializeField] private float accelerationForce;
+    [SerializeField] private float gear1AccelForce;
+    [SerializeField] private float gear2AccelForce;
+    [SerializeField] private float gear3AccelForce;
+    [SerializeField] private float gear4AccelForce;
+    [SerializeField] private float gear1CutoffSpeed;
+    [SerializeField] private float gear2CutoffSpeed;
+    [SerializeField] private float gear3CutoffSpeed;
+    [SerializeField] private float gear4CutoffSpeed;
+
     [SerializeField] private float brakeForce;
     [SerializeField] private float maxSteerAngle;
+    [SerializeField] private float gear1MaxSteerAngle;
+    [SerializeField] private float gear2MaxSteerAngle;
+    [SerializeField] private float gear3MaxSteerAngle;
+    [SerializeField] private float gear4MaxSteerAngle;
 
     [Header("Wheel Colliders")]
     [SerializeField] private WheelCollider wheelFrontLeftCollider;
@@ -30,11 +34,39 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform wheelBackLeftVisual;
     [SerializeField] private Transform wheelBackRightVisual;
 
+
+    [Header("Brake Lights")]
+    [SerializeField] private GameObject brakeLights;
+
+    [Header("Reverse Lights")]
+    [SerializeField] private GameObject reverseLights;
+
+    private Rigidbody rigidBody;
+    private float steeringInput;
+    private float pedalInput;
+    private float currentSteerAngle;
+    private float currentBrakeForce;
+    private bool isBraking = false;
+    private bool isReversing = false;
+    private bool isMoving = false;
+    private bool isAbleToSwitchGears;
+
+    // For floating-point comparison
+    private float epsilon = 0.0001f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis(HORIZONTAL);
-        verticalInput = Input.GetAxis(VERTICAL);
-        isBraking = Input.GetKey(KeyCode.Space);
+        steeringInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        pedalInput = isReversing ? Mathf.Min(verticalInput, 0.0f)
+                                 : Mathf.Max(verticalInput, 0.0f);
+
+        isMoving = IsMoving();
         currentBrakeForce = isBraking ? brakeForce : 0.0f;
 
         HandleMotor();
@@ -43,11 +75,41 @@ public class CarController : MonoBehaviour
         UpdateWheels();
     }
 
+    void Update()
+    {
+        KeyCode brakingKeyCode = isReversing ? KeyCode.W : KeyCode.S;
+        // prevent braking immediately turning to reversing
+        if (isBraking && Input.GetKeyUp(brakingKeyCode) && !isMoving)
+        {
+            isAbleToSwitchGears = true;
+        }
+        else if (isMoving)
+        {
+            isAbleToSwitchGears = false;
+        }
+
+        isBraking = Input.GetKey(brakingKeyCode);
+        reverseLights.SetActive(isReversing);
+        brakeLights.SetActive(!isReversing && isBraking);
+
+        if (isAbleToSwitchGears && Input.GetKey(brakingKeyCode))
+        {
+            isReversing = !isReversing;
+            isAbleToSwitchGears = false;
+        }
+    }
+
+    // Checks if the car is moving
+    public bool IsMoving()
+    {
+        return rigidBody.velocity.magnitude > epsilon;
+    }
+
     private void HandleMotor()
     {
         // apply force only to the front wheels
-        wheelFrontLeftCollider.motorTorque = verticalInput * accelerationForce;
-        wheelFrontRightCollider.motorTorque = verticalInput * accelerationForce;
+        wheelFrontLeftCollider.motorTorque = pedalInput * accelerationForce;
+        wheelFrontRightCollider.motorTorque = pedalInput * accelerationForce;
     }
 
     private void HandleBraking()
@@ -60,7 +122,7 @@ public class CarController : MonoBehaviour
 
     private void HandleSteering()
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
+        currentSteerAngle = maxSteerAngle * steeringInput;
         wheelFrontLeftCollider.steerAngle = currentSteerAngle;
         wheelFrontRightCollider.steerAngle = currentSteerAngle;
     }
