31f72c2107811f0f7e067bcd11fd2d0f0fd1243e
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2021-11-02 14:01:42 +0800

    Make settings work

--- CULLinary2/Assets/Scripts/UI/GameSettingsController.cs
+++ CULLinary2/Assets/Scripts/UI/GameSettingsController.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
-public class GameSettingsController : MonoBehaviour
+public class GameSettingsController : SingletonGeneric<GameSettingsController>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Text currentBgValue;
+    [SerializeField] private TMP_Text currentSfxValue;
+
+    public Resolution[] resolutions;
+
+    private void Start()
     {
-        
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetResolution(int resolutionIndex)
     {
-        
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
+
+    public void SetBGVolume(float volume)
+    {
+        audioMixer.SetFloat("BG_Vol", volume);
+        currentBgValue.text = Mathf.RoundToInt(volume + 80f).ToString();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX_Vol", volume);
+        currentSfxValue.text = Mathf.RoundToInt(volume + 80f).ToString();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
 }
