0673e256aafa25f2ade7badb40cc0ce96b343410
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2021-11-07 17:11:35 +0800

    Fix icon and set cap on consumables

--- CULLinary2/Assets/Scripts/Dungeon/Inventory/PotionLoot.cs
+++ CULLinary2/Assets/Scripts/Dungeon/Inventory/PotionLoot.cs
@@ -4,27 +4,73 @@ using UnityEngine;
 
 public class PotionLoot : Loot
 {
-
     protected override void OnPickup(PlayerPickup playerPickup)
     {
         int randomPotion = Random.Range(1, 6);
-        switch (randomPotion)
+        bool isFull = false;
+        //Hmm... if someone can help me find a better way of doing this
+        if (randomPotion == 1)
         {
-            case 1:
+            if (PlayerManager.instance.healthPill < 99)
+            {
                 PlayerManager.instance.healthPill++;
-                break;
-            case 2:
+            }
+            else
+            {
+                randomPotion = 2;
+            }
+        }
+
+        if (randomPotion == 2)
+        {
+            if (PlayerManager.instance.staminaPill < 99)
+            {
                 PlayerManager.instance.staminaPill++;
-                break;
-            case 3:
+            }
+            else
+            {
+                randomPotion = 3;
+            }
+        }
+
+        if (randomPotion == 3)
+        {
+            if (PlayerManager.instance.potion < 99)
+            {
                 PlayerManager.instance.potion++;
-                break;
-            case 4:
+            }
+            else
+            {
+                randomPotion = 4;
+            }
+        }
+
+        if (randomPotion == 4)
+        {
+            if (PlayerManager.instance.pfizerShot < 99)
+            {
                 PlayerManager.instance.pfizerShot++;
-                break;
-            case 5:
+            }
+            else
+            {
+                randomPotion = 5;
+            }
+        }
+
+        if (randomPotion == 5)
+        {
+            if (PlayerManager.instance.modernaShot < 99)
+            {
                 PlayerManager.instance.modernaShot++;
-                break;
+            }
+            else
+            {
+                isFull = true;
+            }
+        }
+        if (isFull)
+        {
+            return;
         }
         playerPickup.PickUpPotion(randomPotion);
         LootManager.instance.removeLoot(this.gameObject);
