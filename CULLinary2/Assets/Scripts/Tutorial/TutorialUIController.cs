3746989863aa25ed5b68470f32ef3d0562c445ef
alcen <49450743+alcen@users.noreply.github.com>
2021-11-10 00:17:39 +0800

    Add dialogue any key to tutorial

--- CULLinary2/Assets/Scripts/Tutorial/TutorialUIController.cs
+++ CULLinary2/Assets/Scripts/Tutorial/TutorialUIController.cs
@@ -426,6 +426,11 @@ public class TutorialUIController : SingletonGeneric<TutorialUIController>
             {
                 DialogueManager.instance.ShowCannotSkipMessage();
             }
+            else if (isDialogueOpen && Input.anyKeyDown && !(Input.GetMouseButtonDown(0) ||
+                     Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)))
+            {
+                DialogueManager.instance.DisplayNextOnKeyPress();
+            }
             return;
         }
 
