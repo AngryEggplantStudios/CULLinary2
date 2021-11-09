857922ad0ed3839e93649f69a58f06d625cb7323
alcen <49450743+alcen@users.noreply.github.com>
2021-11-09 15:48:45 +0800

    Double money earned from chests with buffs

--- CULLinary2/Assets/Scripts/Dungeon/Inventory/MoneyLoot.cs
+++ CULLinary2/Assets/Scripts/Dungeon/Inventory/MoneyLoot.cs
@@ -8,9 +8,14 @@ public class MoneyLoot : Loot
 
     protected override bool OnPickup(PlayerPickup playerPickup)
     {
-        PlayerManager.instance.currentMoney += moneyAmount;
-        OrdersManager.AddToMoneyEarnedToday(moneyAmount);
-        playerPickup.PickUpMoney(moneyAmount);
+        int moneyAmountReal = moneyAmount;
+        if (OrdersManager.instance != null && OrdersManager.instance.AreEarningsDoubled())
+        {
+            moneyAmountReal *= 2;
+        }
+        PlayerManager.instance.currentMoney += moneyAmountReal;
+        OrdersManager.AddToMoneyEarnedToday(moneyAmountReal);
+        playerPickup.PickUpMoney(moneyAmountReal);
 
         LootManager.instance.removeLoot(this.gameObject);
         Destroy(gameObject);
